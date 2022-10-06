da5c8d7c6f949b621096274474b278c18272f05e
d3cker <l33tdev.marcinkowski@gmail.com>
2022-10-06 13:17:10 +0200

    add Debian Buster, Debian Bullseye and Fedora 36 packages

---
+++ docker/debian_bullseye/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+tar -xzvf wsjtx-patched-2.5.4.tgz
+mkdir build
+cd build
+cmake -DWSJT_SKIP_MANPAGES=ON -DWSJT_GENERATE_DOCS=OFF ../wsjtx-2.5.4
+cmake --build . --target package -- -j2
+
+cp /build/build/wsjtx-prefix/src/wsjtx-build/wsjtx_2.5.4_amd64.deb /build/wsjtx_2.5.4_bullseye_amd64.deb

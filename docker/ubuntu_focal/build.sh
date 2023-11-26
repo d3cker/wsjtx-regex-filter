bf7c98596b9fd41a7249a99fca2c26ceae154c6f
d3cker <l33tdev.marcinkowski@gmail.com>
2023-11-26 14:07:53 +0100

    prepare for new 2.6.1 build and windows support

--- docker/ubuntu_focal/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-tar -xzvf wsjtx-patched-2.5.4.tgz
-mkdir build
-cd build
-cmake -DWSJT_SKIP_MANPAGES=ON -DWSJT_GENERATE_DOCS=OFF ../wsjtx-2.5.4
-cmake --build . --target package -- -j2
-
-cp /build/build/wsjtx-prefix/src/wsjtx-build/wsjtx_2.5.4_amd64.deb /build/wsjtx_2.5.4_focal_amd64.deb

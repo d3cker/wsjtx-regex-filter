93ddfcbc856e99c4db22e9ce483bcf8389eecdf8
d3cker <l33tdev.marcinkowski@gmail.com>
2023-11-26 15:29:11 +0100

    more fixes, using default ubuntu node for patching and downloading source

--- docker/fedora_39/build.sh
+++ docker/fedora_39/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-tar -xzvf wsjtx-patched-2.5.4.tgz
+tar -xzvf wsjtx-patched-2.6.1.tgz
 mkdir build
 cd build
-cmake -DWSJT_SKIP_MANPAGES=ON -DWSJT_GENERATE_DOCS=OFF ../wsjtx-2.5.4
-cmake --build . --target package -- -j2
+cmake -DWSJT_SKIP_MANPAGES=ON -DWSJT_GENERATE_DOCS=OFF ../wsjtx-2.6.1
+cmake --build . --target package -- -j4
 
-cp /build/build/wsjtx-prefix/src/wsjtx-build/wsjtx-2.5.4.x86_64.rpm /build/wsjtx-2.5.4.fc36.x86_64.rpm
+cp /build/build/wsjtx-prefix/src/wsjtx-build/wsjtx-2.6.1.x86_64.rpm /build/wsjtx-2.6.1.fc39.x86_64.rpm
--- docker/ubuntu_jammy/build.sh
+++ docker/ubuntu_jammy/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-tar -xzvf wsjtx-patched-2.5.4.tgz
+tar -xzvf wsjtx-patched-2.6.1.tgz
 mkdir build
 cd build
-cmake -DWSJT_SKIP_MANPAGES=ON -DWSJT_GENERATE_DOCS=OFF ../wsjtx-2.5.4
-cmake --build . --target package -- -j2
+cmake -DWSJT_SKIP_MANPAGES=ON -DWSJT_GENERATE_DOCS=OFF ../wsjtx-2.6.1
+cmake --build . --target package -- -j4
 
-cp /build/build/wsjtx-prefix/src/wsjtx-build/wsjtx_2.5.4_amd64.deb /build/wsjtx_2.5.4_jammy_amd64.deb
+cp /build/build/wsjtx-prefix/src/wsjtx-build/wsjtx_2.6.1_amd64.deb /build/wsjtx_2.6.1_jammy_amd64.deb
